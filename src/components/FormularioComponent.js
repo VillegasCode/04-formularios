a23eed5b0ac82e46558b9f65db777d8a061b4c4d
Carlos Villegas <carlos.villegasch@gmail.com>
2023-05-26 21:08:10 -0500

    cambiarDatos function to show data in realtime on screen

--- src/components/FormularioComponent.js
+++ src/components/FormularioComponent.js
@@ -13,19 +13,36 @@ export const FormularioComponent = () => {
             nombre: datos.nombre.value,
             apellido: datos.apellido.value,
             genero: datos.genero.value,
-            bio: datos.bio.value
+            bio: datos.bio.value,
+            enviar: datos.enviar.value
         };
 
         console.log(usuario);
         setUsuario(usuario);
     }
   
+
+    const cambiarDatos = e => {
+        let name_del_input = e.target.name;
+        let usuario_para_modificar = usuario;
+
+        console.log(usuario_para_modificar);
+
+        setUsuario(estadoprevio => {
+            return{
+                ...estadoprevio,
+                [name_del_input]: e.target.value
+            }
+        });
+    }
+
+
     return (
     <div>
         <h1>Formularios con React</h1>
 
         {/* Mostrar todos los datos por pantalla con un IF CONDICIONAL PREVIO */}
-        { usuario.bio && usuario.bio.length >= 2 &&
+        { usuario.enviar && 
             (
             <div className='info_usuario label label-gray'>
                 {usuario.nombre} {usuario.apellido} es {usuario.genero} y su biografía es esta: <p>{usuario.bio}</p>
@@ -34,15 +51,15 @@ export const FormularioComponent = () => {
         }
 
         <form onSubmit={conseguirDatosFormulario}>
-            <input type='text' placeholder='Nombre' name='nombre'/>
-            <input type='text' placeholder='Apellido' name='apellido'/>
-            <select name='genero'>
+            <input type='text' placeholder='Nombre' name='nombre' onChange={cambiarDatos}/>
+            <input type='text' placeholder='Apellido' name='apellido' onChange={cambiarDatos}/>
+            <select name='genero' onChange={cambiarDatos}>
                 <option value="hombre">Hombre</option>
                 <option value="mujer">Mujer</option>
             </select>
-            <textarea placeholder='Biografia' name='bio'></textarea>
+            <textarea placeholder='Biografia' name='bio' onChange={cambiarDatos}></textarea>
 
-            <input type='submit' value="Enviar" />
+            <input type='submit' value="Enviar" name='enviar'/>
         </form>
     </div>
     
